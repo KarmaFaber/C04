21dc88efc9c839e26ce426d7a248d69f2b000ef4
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-08 07:48:05 +0200

    ex00

---
+++ ft_strlen.c
@@ -0,0 +1,19 @@
+int ft_strlen(char *str)
+{
+   int i;
+   
+   i == 0;
+   while (str[i] != '\0')
+    {
+        i ++ ;
+    }
+    return i;
+}
+
+#include <stdio.h>
+int main(void)
+{
+    int len =ft_strlen("hola caracola loca");
+    printf("%i\n", len);
+    return 0;
+}
